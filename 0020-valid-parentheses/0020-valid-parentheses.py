0332981e17e21281abc7448cc830ebbd14087af0
ADITYA VIKRAM SINGH <adityaprinshu2004@gmail.com>
2025-04-22 14:52:22 +0530

    Time: 0 ms (100%), Space: 17.8 MB (81.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        closetotop = {"}" : "{", "]" : "[", ")" : "("}
+        dictionary = {"}":"{", "]":"[", ")":"("}
 
-        for c in s:
-            if c in closetotop:
-                if stack and stack[-1] == closetotop[c]:
+        for char in s:
+            if char in dictionary:
+                if stack and stack[-1] == dictionary[char]:
                     stack.pop()
                 else:
                     return False
             else:
-                stack.append(c)
-
+                stack.append(char)
         return True if not stack else False
         
